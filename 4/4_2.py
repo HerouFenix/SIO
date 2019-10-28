f3404b43e9ef0e3c635cd2570f775ed1cd41b8e4
HerouFenix <diogo04@ua.pt>
2019-10-28 15:58:58 +0000

    started p4

---
+++ 4/4_2.py
@@ -0,0 +1,44 @@
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+
+cert_dic = {}
+
+def get_certificate_dates(certificate):
+    dates = (certificate.not_valid_before,certificate.not_valid_after)
+    print("Before date: ", dates[0], " ; After date: ", dates[1],"\n")
+    return dates
+
+def load_certificate(file_path):
+    try:
+        with open(file_path,"rb") as reader:
+            cert_data = reader.read()
+
+        cert = x509.load_pem_x509_certificate(cert_data, default_backend())
+
+        cert_dic[cert.subject.rfc4514_string()] = cert
+    except Exception as e:
+        print("Error: ",e,"\n")
+
+
+
+def main():
+    file_path = input("Write the certificate's path file or write nothing to stop: \n > ")
+    if len(file_path) == 0:
+        return 1
+    
+    load_certificate(file_path)
+
+    return 0
+    
+
+    
+
+if __name__ == '__main__':
+    while (True):
+        if main() == 1:
+            break
+
+    for i in cert_dic:
+        get_certificate_dates(cert_dic[i])
+        
+    print("Bye bye\n")
